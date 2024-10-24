03420a1b66a0fc00ddb32efa2002225d13cc1977
HERITIER Quentin <quentheritier@gmail.com>
2024-10-24 16:50:02 +0200

    Added css and search on the cocktail app

--- JS/Bases/Asynchrone/mini_projet/main.js
+++ JS/Bases/Asynchrone/mini_projet/main.js
@@ -1,5 +1,7 @@
 let searchForm = document.querySelector('#searchForm');
 let monInput = document.querySelector('#monInput');
+let resultContainer = document.querySelector('#resultContainer');
+
 searchForm.addEventListener('submit', (evt) => {
     evt.preventDefault();
     const query = monInput.value;
@@ -8,16 +10,51 @@ searchForm.addEventListener('submit', (evt) => {
 
 async function obtenirDonnées(query) {
     try {
-        const response = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${query}`);
-
-        // Si il la donnée n'est pas flagué par un ok alors création d'erreur
-        // if (!response.ok) {
-        //     throw new Error(response.status);
-        // }
-
+        const response = await fetch(
+            `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${query}` &&
+                `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${query}`
+        );
         const data = await response.json();
         console.log(data);
+
+        resultContainer.innerHTML = '';
+        if (data.drinks) {
+            displaySearch(data.drinks);
+        }
     } catch (error) {
         console.error('Erreur : ' + error);
     }
+}
+
+function displaySearch(drinks) {
+    drinks.forEach((cocktail) => {
+        // Créer une section pour chaque cocktail
+        let cocktailSection = document.createElement('section');
+        cocktailSection.classList.add('container__cocktail');
+
+        // Ajoute une sectionpour le nom et la description
+        let nameSection = document.createElement('section');
+        nameSection.classList.add('container__name')
+
+        // Ajouter un titre et une image
+        let cocktailName = document.createElement('h3');
+        cocktailName.textContent = cocktail.strDrink;
+
+        let cocktailImage = document.createElement('img');
+        cocktailImage.src = cocktail.strDrinkThumb;
+        cocktailImage.alt = cocktail.strDrink;
+
+        // Ajouter description
+        let cocktailDescription = document.createElement('p');
+        cocktailDescription.textContent = cocktail.strInstructions;
+        
+        // Ajouter la section au conteneur
+        resultContainer.appendChild(cocktailSection);
+        
+        // Ajouter les éléments à la section
+        cocktailSection.appendChild(cocktailImage);
+        cocktailSection.appendChild(nameSection);
+        nameSection.appendChild(cocktailName);
+        nameSection.appendChild(cocktailDescription);
+    });
 }
