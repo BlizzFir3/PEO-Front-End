a9faf513684deb3382a634363c707ce3c2143bb7
BlizzFir3 <111078772+BlizzFir3@users.noreply.github.com>
2024-11-05 15:47:31 +0100

    added few option to change text size etc

---
+++ Accesibility/project/Labo/main.js
@@ -0,0 +1,42 @@
+const body = document.querySelector("body");
+const btnDys = document.getElementById("btn-dys");
+const p = document.querySelector("p");
+// zoom
+function zoomFont() {
+    const size = +body.style.fontSize.split("rem").join("");
+    body.style.fontSize = size + 0.2 + "rem";
+}
+// unzoom
+function unZoomFont() {
+    const size = +body.style.fontSize.split("rem").join("");
+    body.style.fontSize = size - 0.2 + "rem";
+}
+// zoomLineHeigth
+function zoomLineHeigth() {
+    const size = +body.style.lineHeight.split("rem").join("");
+    body.style.lineHeight = size + 0.2 + "rem";
+}
+// zoomLineHeigth
+function unZoomLineHeigth() {
+    const size = +body.style.lineHeight.split("rem").join("");
+    body.style.lineHeight = size - 0.2 + "rem";
+}
+// setDysFont
+function setDysFont() {
+    body.classList.toggle("dyslexia");
+    body.classList.contains("dyslexia")
+        ? (btnDys.textContent = "Police classique")
+		: (btnDys.textContent = "Police Dys");
+	p.classList.toggle("dyslexia");
+    p.classList.contains("dyslexia")
+        ? (btnDys.textContent = "Police classique")
+        : (btnDys.textContent = "Police Dys");
+}
+
+// reset
+function reset() {
+    body.classList.remove("contrast");
+    body.classList.remove("dyslexia");
+    body.style.fontSize = "1rem";
+	body.style.lineHeight = "1.5rem";
+}
