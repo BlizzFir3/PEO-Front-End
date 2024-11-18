f39ce0f35b4673a6e8a089d523b7e3bbbad6fd8e
HERITIER Quentin <quentheritier@gmail.com>
2024-11-18 09:44:38 +0100

    create portée.js

---
+++ ReactJS/portee.js
@@ -0,0 +1,16 @@
+function example() {
+    if (true) {
+        var myVar = 5;
+        let myLet = 5;
+        const MYCONST = 5;
+
+        console.log('myVar', myVar);
+        console.log('myLet', myLet);
+        console.log('MYCONST', MYCONST);
+    }
+    console.log('myVar', myVar);
+    // console.log('myLet', myLet);
+    // console.log('MYCONST', MYCONST);
+}
+
+example();
