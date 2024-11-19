4d7d33b4792e65c8307765a5167c989d00c69f35
BlizzFir3 <111078772+BlizzFir3@users.noreply.github.com>
2024-11-19 10:04:59 +0100

    imported routes, route

--- ReactJS/18112024_React/FirstProject/src/App.jsx
+++ ReactJS/18112024_React/FirstProject/src/App.jsx
@@ -1,12 +1,16 @@
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 
 function App() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<>Ma Page d'accueil</>} />
-                <Route path="/test" element={<>Page Test</>} />
+                <Route path="/" element={<>Ma page d'accueil</>} />
+                <Route
+                    path="/test"
+                    style={{ color: 'white' }}
+                    element={<h1>page TEST</h1>}
+                />
             </Routes>
         </BrowserRouter>
     );
