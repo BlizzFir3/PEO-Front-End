ae0e29711c3f85315a821c3ee491b95b1fbfd946
BlizzFir3 <111078772+BlizzFir3@users.noreply.github.com>
2024-11-19 11:02:27 +0100

    usage of useState

--- ReactJS/18112024_React/FirstProject/src/components/views/HookUseSate.jsx
+++ ReactJS/18112024_React/FirstProject/src/components/views/HookUseSate.jsx
@@ -1,5 +1,30 @@
+import { useState } from "react";
+
+/**
+ * This function demonstrates the usage of the useState hook in a React component.
+ * It maintains a count state and provides an increment function to update the count.
+ *
+ * @returns {JSX.Element} - A React component that displays the current count and a button to increment it.
+ */
 function HookUseState() {
-    return <h1>Le hook useState</h1>;
+	const [count, setCount] = useState(0);
+
+    /**
+     * Increments the count state by 1 and logs the current count to the console.
+     */
+    const increment = () => {
+        setCount(count + 1);
+        console.log(count);
+    };
+
+    return (
+        <>
+            <h1>Le hook useState</h1>
+            <p>{count}</p>
+            <button onClick={increment}> + </button>
+        </>
+    );
 }
+
 
 export default HookUseState;
