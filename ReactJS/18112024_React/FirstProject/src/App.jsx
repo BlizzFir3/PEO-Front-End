5b3c1653e13cceb9da11e5f1da1e539a81ee573e
BlizzFir3 <111078772+BlizzFir3@users.noreply.github.com>
2024-11-19 09:28:21 +0100

    ended component app

--- ReactJS/18112024_React/FirstProject/src/App.jsx
+++ ReactJS/18112024_React/FirstProject/src/App.jsx
@@ -1,20 +1,28 @@
-import { Fragment } from "react";
-import "./App.css";
+import { Fragment } from 'react';
+import './App.css';
 
 function App() {
-    let user = { name: "John", email: "john@gmail.com" };
+    let user = { name: 'John', email: 'john@gmail.com' };
     let user2 = undefined;
-    let salut = "Salut !";
+    let salut = 'Salut !';
     let title =
-        "Bonjour <strong>Tout le monde</strong> Bienvenu sur notre appli";
-    const imgSrc = "../src/assets/react.svg";
-    const style = { color: "blue", backgroundColor: "white" };
+        'Bonjour <strong>Tout le monde</strong> Bienvenu sur notre appli';
+    const imgSrc = '../src/assets/react.svg';
+    const style = { color: 'blue', backgroundColor: 'white' };
 
     const display = () => {
         if (user) {
             return <p>{user.name}</p>;
         } else return <p>Utilisateur non trouvé</p>;
     };
+
+    function handleClick(e) {
+        console.log(e);
+        alert("J'ai clique !");
+    }
+
+    const fruits = ['banane', 'pomme', 'abricot', 'poire'];
+
     return (
         <>
             <p>{salut}</p>
@@ -29,13 +37,23 @@ function App() {
             <section style={style}>
                 <p>Je suis la div avec le style</p>
             </section>
-			<h2>Utilisateur : {display()}</h2>
-			{/* Affiche nom si vrai sinon Undefined */}
+            <h2>Utilisateur : {display()}</h2>
+            {/* Affiche nom si vrai sinon Undefined */}
             {user2 ? <h2>{user2.name}</h2> : <h2>Undefined</h2>}
-			{/* Affiche si vrai sinon affiche rien */}
-			{user2 && <h2>{user2.name}</h2>}
+            {/* Affiche si vrai sinon affiche rien */}
+            {user2 && <h2>{user2.name}</h2>}
+            <button onClick={handleClick}>Cliquez !</button>
+            <button onClick={() => alert("J'ai encore cliquer")}>
+                Cliquez encore !
+            </button>
+            <ul>
+                {fruits.map((fruit) => (
+                    <li key={fruit}>{fruit}</li>
+                ))}
+            </ul>
         </>
     );
 }
 
 export default App;
+
