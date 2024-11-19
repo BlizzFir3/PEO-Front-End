db5ed651b180bb58216e8c89735c6c8ee61959e9
BlizzFir3 <111078772+BlizzFir3@users.noreply.github.com>
2024-11-19 15:24:41 +0100

    done second task

--- ReactJS/18112024_React/FirstProject/src/components/views/Exercice.jsx
+++ ReactJS/18112024_React/FirstProject/src/components/views/Exercice.jsx
@@ -11,6 +11,10 @@ function Exercice() {
         { category: 'Vegetables', price: '$1', number: 6, name: 'Zucchini' },
     ];
 
+    // Filtrage des aliments par category ainsi que number (en stock ou non)
+    let fruitsInStock = products.filter((fruit) => {
+        return fruit.category === 'Fruits' && fruit.number !== 0;
+    });
     let fruits = products.filter((fruit) => {
         return fruit.category === 'Fruits';
     });
@@ -18,17 +22,27 @@ function Exercice() {
     let vegetables = products.filter((fruit) => {
         return fruit.category === 'Vegetables';
     });
+    let vegetablesInStock = products.filter((fruit) => {
+        return fruit.category === 'Vegetables' && fruit.number !== 0;
+    });
 
-    const [checked, setChecked] = useState(false);
+    const [checkedFruit, setCheckedFruit] = useState(false);
+    const [checkedVegetable, setCheckedVegetable] = useState(false);
 
     return (
         <>
             <h1 className="pt-20">Exercice</h1>
             <Consigne />
-            <h2 className='pt-10'>Fruits</h2>
+            <h2 className="pt-10">Fruits</h2>
+            <div className="form-control">
+                <input
+                    type="text"
+                    placeholder="Rechercher un fruit"
+                    className="input input-bordered w-24 md:w-auto"
+                />
+            </div>
             <div className="overflow-x-auto">
                 <table className="table">
-                    {/* head */}
                     <thead>
                         <tr>
                             <th scope="col">Nom</th>
@@ -36,19 +50,30 @@ function Exercice() {
                             <th scope="col">Quantité</th>
                         </tr>
                     </thead>
-                    <FruitOuLegumes data={fruits} />
+                    {checkedFruit ? (
+                        <FruitOuLegumes data={fruitsInStock} />
+                    ) : (
+                        <FruitOuLegumes data={fruits} />
+                    )}
                 </table>
             </div>
             <div className="p-10">
-                <CheckBoxEnfant checked={checked} onCheck={setChecked} />
-                {checked
-                    ? 'Afficher les produits non disponible'
-                    : 'Ne pas afficher les produits non disponible'}
+                <p>Masquer les fruits non disponible : </p>
+                <CheckBoxFruit
+                    checked={checkedFruit}
+                    onCheck={setCheckedFruit}
+                />
             </div>
-            <h2>Vegetables</h2>
+            <h2>Legumes</h2>
+            <div className="form-control">
+                <input
+                    type="text"
+                    placeholder="Rechercher un legume"
+                    className="input input-bordered w-24 md:w-auto"
+                />
+            </div>
             <div className="overflow-x-auto">
                 <table className="table">
-                    {/* head */}
                     <thead>
                         <tr>
                             <th scope="col">Nom</th>
@@ -56,15 +81,20 @@ function Exercice() {
                             <th scope="col">Quantité</th>
                         </tr>
                     </thead>
-                    <FruitOuLegumes data={vegetables} />
+                    {checkedVegetable ? (
+                        <FruitOuLegumes data={vegetablesInStock} />
+                    ) : (
+                        <FruitOuLegumes data={vegetables} />
+                    )}
                 </table>
             </div>
-                <div className="p-10">
-                    <CheckBoxEnfant checked={checked} onCheck={setChecked} />
-                    {checked
-                        ? 'Afficher les produits non disponible'
-                        : 'Ne pas afficher les produits non disponible'}
-                </div>
+            <div className="p-10">
+                <p>Masquer les legumes non disponible :</p>
+                <CheckBoxVegetable
+                    checked={checkedVegetable}
+                    onCheck={setCheckedVegetable}
+                />
+            </div>
         </>
     );
 }
@@ -81,7 +111,19 @@ const FruitOuLegumes = ({ data }) => {
     ));
 };
 
-const CheckBoxEnfant = ({ checked, onCheck }) => {
+const CheckBoxFruit = ({ checked, onCheck }) => {
+    const handleClick = (e) => {
+        onCheck(e.target.checked);
+    };
+
+    return (
+        <>
+            <input type="checkbox" onChange={handleClick} checked={checked} />
+        </>
+    );
+};
+
+const CheckBoxVegetable = ({ checked, onCheck }) => {
     const handleClick = (e) => {
         onCheck(e.target.checked);
     };
