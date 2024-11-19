b182a2445f7ef07af3e6cec2991a514b8827c158
BlizzFir3 <111078772+BlizzFir3@users.noreply.github.com>
2024-11-19 13:48:59 +0100

    Tets

--- ReactJS/18112024_React/FirstProject/src/components/views/Flux.jsx
+++ ReactJS/18112024_React/FirstProject/src/components/views/Flux.jsx
@@ -1,6 +1,5 @@
 function Flux() {
-
-	const data = [
+    const data = [
         { id: 1, nom: 'Produit 1', prix: 20.99, quantite: 5 },
         { id: 2, nom: 'Produit 2', prix: 15.45, quantite: 10 },
         { id: 3, nom: 'Produit 3', prix: 75.32, quantite: 58 },
@@ -8,7 +7,33 @@ function Flux() {
         { id: 5, nom: 'Produit 5', prix: 26.0, quantite: 16 },
     ];
 
-	return <h1>Flux</h1>
+    return (
+        <>
+            <h1 className="pt-20">Les flux de donnees</h1>
+            <h2>De parents vers enfants</h2>
+            <p>
+                Pour faire passer des donnees d'un composant parent vers le
+                composant enfant, nous devons utiliser les props
+            </p>
+            <ComposantEnfant data={data} />
+            <h2>De l'enfant vers le parent</h2>
+            <p>
+                En React, pour faire passer des donnees d'un composant enfant
+                vers son parent, il est courant d'utiliser des fonctions de
+                rappel (callbacks)
+            </p>
+        </>
+    );
 }
+
+const ComposantEnfant = ({ data }) => {
+    return data.map((produit) => (
+        <div key={produit.id}>
+            <p>nom : {produit.nom}</p>
+            <p>prix : {produit.prix}</p>
+            <p>quantite : {produit.quantite}</p>
+        </div>
+    ));
+};
 
 export default Flux;
