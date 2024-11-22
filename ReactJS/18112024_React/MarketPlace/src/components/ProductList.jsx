a24cf4a527db368ce3dc95ff3534112daed824ae
HERITIER Quentin <quentheritier@gmail.com>
2024-11-22 09:36:58 +0100

    added hook

---
+++ ReactJS/18112024_React/MarketPlace/src/components/ProductList.jsx
@@ -0,0 +1,7 @@
+export default function ProductList({products}) {
+    return (
+        <>
+            <h1>produits</h1>
+        </>
+    );
+}
--- ReactJS/18112024_React/MarketPlace/src/components/views/Products.jsx
+++ ReactJS/18112024_React/MarketPlace/src/components/views/Products.jsx
@@ -1,9 +1,16 @@
+import { useEffect, useState } from "react";
+import ProductList from "../ProductList";
 import Titre from "../Titre";
 
 export default function Products() {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {},[]);
+
     return (
         <>
             <Titre>Mes Produits</Titre>
+            <ProductList products={products} />
         </>
     );
 }
